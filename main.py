6374598abb08d7129a6bcb1832677a779d47b05d
micfun123 <54761507+micfun123@users.noreply.github.com>
2022-08-02 14:38:35 +0100

    above ground

--- main.py
+++ main.py
@@ -9,12 +9,18 @@ import os
 import ssl
 import time
 
+from sqlalchemy import true
+
 while True:
     TrainDelaysAPIURl = "https://api.tfl.gov.uk/line/mode/tube/status"
+    Londonoverground = "https://api.tfl.gov.uk/Line/london-overground/stoppoints"
     data = requests.get(TrainDelaysAPIURl)
     data = data.json()  
+    londonabovegrounddata = requests.get(Londonoverground)
+    londonabovegrounddata = londonabovegrounddata.json()
 
     Lines = []  
+    abovegroundLines = []
 
     for i in range(len(data)):
         name = data[i]["name"]
@@ -22,6 +28,17 @@ while True:
         status = data[i]["lineStatuses"][0]["statusSeverityDescription"]
         summary = f"{name} has these problems: {problems}. It is {status}."
         Lines.append(summary)   
+
+    for i in range(len(londonabovegrounddata)):
+        name = londonabovegrounddata[i]["commonName"]
+        status = londonabovegrounddata[i]["status"]
+        print(name, status)
+        if status == True:
+            status = "Operational"
+        else:
+            status = "Not Operational"
+        summary = f"{name} is {status}."
+        abovegroundLines.append(summary)
 
     print(Lines)
     #email users lines  
@@ -31,8 +48,8 @@ while True:
     fromuser = os.environ["GMAIL_USER"]
     touser = users  
 
-    subject = "Tube Status"
-    body = "\n".join(Lines) 
+    subject = "Trains in London"
+    body = "\n".join(Lines) + "\n\n" + "\n".join(abovegroundLines)
 
     em = EmailMessage()
     em.set_content(body)
